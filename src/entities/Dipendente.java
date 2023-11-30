80de9616050a357774a70795443674bd9d9ada48
AndreaMarchica <andrea.marchica94@gmail.com>
2023-11-30 16:59:53 +0100

    executing ex 1

---
+++ src/entities/Dipendente.java
@@ -0,0 +1,72 @@
+package entities;
+
+public class Dipendente {
+    /*ATTRIBUTI*/
+    public static double stipendioBase = 1000;
+    private static int matricola;
+    private static double stipendio;
+    private static double importoOrarioStraordinario;
+    public enum Livello {OPERAIO, IMPIEGATO, QUADRO, DIRIGENTE};
+    public enum Dipartimento {PRODUZIONE, AMMINISTRAZIONE, VENDITE};
+    private Livello livello;
+    private Dipartimento dipartimento;
+
+    /*COSTRUTTORI*/
+
+    public Dipendente(int matricola, Dipartimento dipartimento) {
+        this.matricola = matricola;
+        this.dipartimento = dipartimento;
+        this.stipendio = stipendioBase;
+        this.importoOrarioStraordinario = 30;
+        this.livello = Livello.OPERAIO;
+    }
+    public Dipendente(int matricola, Dipartimento dipartimento, double importoOrarioStraordinario, Livello livello){
+        this.matricola = matricola;
+        this.dipartimento = dipartimento;
+        this.stipendio = stipendioBase;
+        this.importoOrarioStraordinario = importoOrarioStraordinario;
+        this.livello = livello;
+    }
+
+    /*METODI*/
+    @Override
+    public String toString() {
+        return "Matricola: " + matricola +
+                " Dipartimento: " + dipartimento +
+                " Stipendio Base: " + stipendioBase +
+                " Stipendio: " + stipendio +
+                " Importo Orario Straordinario: " + importoOrarioStraordinario +
+                " Livello: " + livello;
+    }
+    public void promuovi(){
+        if(this.livello == Dipendente.Livello.OPERAIO){
+            this.livello = Livello.IMPIEGATO;
+            this.stipendio = stipendioBase * 1.2;
+        } if(this.livello == Livello.IMPIEGATO){
+            this.livello = Livello.QUADRO;
+            this.stipendio = stipendioBase * 1.5;
+        } if(this.livello == Livello.QUADRO){
+            this.livello = Livello.DIRIGENTE;
+            this.stipendio = stipendioBase * 2;
+        } else System.out.println("Sei già il capo");
+    }
+    public void calcolaPaga(Dipendente dipendente){
+        System.out.println(dipendente.stipendio);
+    }
+    /*   getter*/
+    public double getStipendioBase() {
+        return stipendioBase;
+    }
+
+    public int getMatricola() {
+        return matricola;
+    }
+
+    public double getStipendio() {
+        return stipendio;
+    }
+
+    public double getImportoOrarioStraordinario() {
+        return importoOrarioStraordinario;
+    }
+}
